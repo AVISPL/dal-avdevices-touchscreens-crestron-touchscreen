957731827fb7846ee08baa768e4167273b453369
ThanhD-AVISPL <thanh.do@avispl.com>
2025-11-17 16:24:57 +0700

    [SYAL-2578] [Tech] Add Display Information

--- src/test/java/com/avispl/symphony/dal/avdevices/touchscreens/crestron/touchscreen/CrestronTouchPanelCommunicatorTest.java
+++ src/test/java/com/avispl/symphony/dal/avdevices/touchscreens/crestron/touchscreen/CrestronTouchPanelCommunicatorTest.java
@@ -10,8 +10,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
 import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
 import com.avispl.symphony.dal.avdevices.touchscreens.crestron.touchscreen.common.constants.Constant;
+import com.avispl.symphony.dal.avdevices.touchscreens.crestron.touchscreen.types.DisplayEdge;
+import com.avispl.symphony.dal.avdevices.touchscreens.crestron.touchscreen.types.properties.Display;
 
 /**
  * Unit tests for the {@link CrestronTouchPanelCommunicator} class.
@@ -48,6 +51,39 @@ class CrestronTouchPanelCommunicatorTest {
 		this.verifyStatistics(statistics);
 	}
 
+	@Test
+	void testControlPropertyWithDropdown() throws Exception {
+		String propertyName = String.format(Constant.PROPERTY_FORMAT, Constant.DISPLAY_GROUP, Display.BUTTON_TOOLBAR_DISPLAY_EDGE.getName());
+		String value = DisplayEdge.LEFT.getValue();
+		ControllableProperty controllableProperty = new ControllableProperty();
+		controllableProperty.setProperty(propertyName);
+		controllableProperty.setValue(value);
+
+		this.communicator.controlProperty(controllableProperty);
+		this.extendedStatistics = (ExtendedStatistics) this.communicator.getMultipleStatistics().get(0);
+		Map<String, String> statistics = this.extendedStatistics.getStatistics();
+		String verifiedValue = statistics.get(propertyName);
+		Assertions.assertNotNull(verifiedValue, "Can't found the property name %s".formatted(propertyName));
+		Assertions.assertEquals(value, verifiedValue, "The value of %s does not equals".formatted(propertyName));
+	}
+
+	@Test
+	void testControlPropertyWithSwitch() throws Exception {
+		String propertyName = String.format(Constant.PROPERTY_FORMAT, Constant.DISPLAY_GROUP, Display.AUDIO_PANEL_MUTE.getName());
+		String value = "0";
+		ControllableProperty controllableProperty = new ControllableProperty();
+		controllableProperty.setProperty(propertyName);
+		controllableProperty.setValue(value);
+
+		this.communicator.controlProperty(controllableProperty);
+		this.extendedStatistics = (ExtendedStatistics) this.communicator.getMultipleStatistics().get(0);
+		Map<String, String> statistics = this.extendedStatistics.getStatistics();
+		String verifiedValue = statistics.get(propertyName);
+		String comparedValue = "1".equals(value) ? Constant.ON : Constant.OFF;
+		Assertions.assertNotNull(verifiedValue, "Can't found the property name %s".formatted(propertyName));
+		Assertions.assertEquals(comparedValue, verifiedValue, "The value of %s does not equals".formatted(propertyName));
+	}
+
 	private void verifyStatistics(Map<String, String> statistics) {
 		Map<String, Map<String, String>> groups = new LinkedHashMap<>();
 		groups.put(Constant.GENERAL_GROUP, this.filterGroupStatistics(statistics, null));
@@ -55,6 +91,7 @@ class CrestronTouchPanelCommunicatorTest {
 		groups.put(Constant.CAPABILITIES_GROUP, this.filterGroupStatistics(statistics, Constant.CAPABILITIES_GROUP));
 		groups.put(Constant.SYSTEM_VERSIONS_GROUP, this.filterGroupStatistics(statistics, Constant.SYSTEM_VERSIONS_GROUP));
 		groups.put(Constant.NETWORK_GROUP, this.filterGroupStatistics(statistics, Constant.NETWORK_GROUP));
+		groups.put(Constant.DISPLAY_GROUP, this.filterGroupStatistics(statistics, Constant.DISPLAY_GROUP));
 
 		for (Map<String, String> initGroup : groups.values()) {
 			for (Map.Entry<String, String> initStatistics : initGroup.entrySet()) {
