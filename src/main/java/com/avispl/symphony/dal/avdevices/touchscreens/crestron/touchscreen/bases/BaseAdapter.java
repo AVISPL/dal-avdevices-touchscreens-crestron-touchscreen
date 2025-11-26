4bc58f9459776a1e9ad745ea088f5a9c354abc23
ThanhD-AVISPL <thanh.do@avispl.com>
2025-11-26 15:41:24 +0700

    [SYAL-2702] The units of measurement are not displayed for the CurrentValue properties

--- src/main/java/com/avispl/symphony/dal/avdevices/touchscreens/crestron/touchscreen/bases/BaseAdapter.java
+++ src/main/java/com/avispl/symphony/dal/avdevices/touchscreens/crestron/touchscreen/bases/BaseAdapter.java
@@ -3,6 +3,8 @@ package com.avispl.symphony.dal.avdevices.touchscreens.crestron.touchscreen.base
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * The template for all network adapter.
@@ -11,6 +13,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @since 1.0.0
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 public abstract class BaseAdapter {
 	@JsonProperty("DomainName")
 	private String domainName;
@@ -18,58 +22,4 @@ public abstract class BaseAdapter {
 	private Boolean linkStatus;
 	@JsonProperty("MacAddress")
 	private String macAddress;
-
-	/**
-	 * Retrieves {@link #domainName}
-	 *
-	 * @return value of {@link #domainName}
-	 */
-	public String getDomainName() {
-		return domainName;
-	}
-
-	/**
-	 * Sets {@link #domainName} value
-	 *
-	 * @param domainName new value of {@link #domainName}
-	 */
-	public void setDomainName(String domainName) {
-		this.domainName = domainName;
-	}
-
-	/**
-	 * Retrieves {@link #linkStatus}
-	 *
-	 * @return value of {@link #linkStatus}
-	 */
-	public Boolean getLinkStatus() {
-		return linkStatus;
-	}
-
-	/**
-	 * Sets {@link #linkStatus} value
-	 *
-	 * @param linkStatus new value of {@link #linkStatus}
-	 */
-	public void setLinkStatus(Boolean linkStatus) {
-		this.linkStatus = linkStatus;
-	}
-
-	/**
-	 * Retrieves {@link #macAddress}
-	 *
-	 * @return value of {@link #macAddress}
-	 */
-	public String getMacAddress() {
-		return macAddress;
-	}
-
-	/**
-	 * Sets {@link #macAddress} value
-	 *
-	 * @param macAddress new value of {@link #macAddress}
-	 */
-	public void setMacAddress(String macAddress) {
-		this.macAddress = macAddress;
-	}
 }
--- src/main/java/com/avispl/symphony/dal/avdevices/touchscreens/crestron/touchscreen/types/properties/Network.java
+++ src/main/java/com/avispl/symphony/dal/avdevices/touchscreens/crestron/touchscreen/types/properties/Network.java
@@ -1,6 +1,8 @@
 /** Copyright (c) 2025 AVI-SPL, Inc. All Rights Reserved. */
 package com.avispl.symphony.dal.avdevices.touchscreens.crestron.touchscreen.types.properties;
 
+import lombok.Getter;
+
 import com.avispl.symphony.dal.avdevices.touchscreens.crestron.touchscreen.bases.BaseProperty;
 
 /**
@@ -9,6 +11,7 @@ import com.avispl.symphony.dal.avdevices.touchscreens.crestron.touchscreen.bases
  * @author Kevin / Symphony Dev Team
  * @since 1.0.0
  */
+@Getter
 public enum Network implements BaseProperty {
 	DNS_SERVERS("DNSServers"),
 	HOSTNAME("Hostname"),
@@ -29,15 +32,5 @@ public enum Network implements BaseProperty {
 
 	Network(String name) {
 		this.name = name;
-	}
-
-	/**
-	 * Retrieves {@link #name}
-	 *
-	 * @return value of {@link #name}
-	 */
-	@Override
-	public String getName() {
-		return name;
 	}
 }
